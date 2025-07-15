d3657140538452ce9a8227210058460a55410785
hosseini@hosseini-To-be-filled-by-O-E-M <hosseini@hosseini-To-be-filled-by-O-E-M>
2025-07-15 11:57:22 +0330

    multiform

--- src/pages/MultiStepForm.tsx
+++ src/pages/MultiStepForm.tsx
@@ -11,36 +11,49 @@ type FormData = {
 export default function MultiStepForm() {
 
     const [step, setStep] = useState(1);
-    const { register, handleSubmit,getValues,trigger,formState: { errors } } = useForm<FormData>();
+    const [complete, setComplete] = useState<{ [key: number]: boolean }>({});
+    const { register, handleSubmit, getValues, trigger, formState: { errors } } = useForm<FormData>();
 
     const onSubmit: SubmitHandler<FormData> = (data) => {
         console.log(data);
-
+        setComplete(prev => ({ ...prev, [3]: true })); 
+        setStep(4); 
     }
-    const nextStep = async(s: number) => {
-        
-        let valid=false;
+    const nextStep = async (s: number) => {
 
-        if(s===1){
-            valid=await trigger("location");
-            console.log("اعتبارسنجی مرحله 1:", valid);
+        let valid = false;
+
+        if (s === 1) {
+            valid = await trigger("location");
+            console.log("validation step1:", valid);
+        } else if (s === 2) {
+            valid = await trigger("roles");
+            console.log("validation step2:", valid);
         }
-        if(valid){
-           // setComplete()
+        /*else if (s === 3) {
+            const isNameValid = await trigger("name");
+            const isPhoneValid = await trigger("phone");
+            valid = isNameValid && isPhoneValid;
+            console.log("validation step3:", valid);
+        }*/
+        if (valid) {
+            setComplete(prev => ({ ...prev, [s]: true }));
+            //setStep((prev) => prev + 1);
             setStep(s + 1);
-        }else{
-            alert("لطفاً فیلدهای این مرحله را کامل و صحیح پر کنید.");
+            console.log(complete);
+        } else {
+            //  alert("لطفاً فیلدهای این مرحله را کامل و صحیح پر کنید.");
         }
-       
-       // const values = getValues();
-//console.log(values);  
+
+        // const values = getValues();
+        //console.log(values);  
     }
     const prevStep = (s: number) => {
+        setComplete(prev => ({ ...prev, [s]: false })); 
         setStep(s - 1);
+        console.log(complete);
     }
-    /* const nextStep = () => {
-         setStep((prev) => prev + 1);
-       };*/
+
 
     return (
         <div className='container0'>
@@ -50,27 +63,41 @@ export default function MultiStepForm() {
                 <div className="step-wrapper">
 
                     <div className='step-container'
-                        onClick={() => setStep(1)}
-                        style={{ color: step === 1 ? "#3498db" : "#ccc" }}
+                        /*  onClick={() => setStep(1)} */
+                        style={{ color: step === 1 ? "#3498db" : complete[1] ? "#000" : "#ccc" }}
                     >
                         <div className='step-style'
-                            style={{ background: step === 1 ? "#3498db" : "#ccc", borderColor: step === 1 ? "#3498db" : "#ccc" }}
-                        >1</div>
+                            style={{
+                                background: step === 1 ? "#3498db" : complete[1] ? "green" : "#ccc",
+                                borderColor: step === 1 ? "#3498db" : complete[1] ? "green" : "#ccc"
+                            }}
+                        >{complete[1] ? "✓" : 1}
+                        </div>
                         <span>Job Location</span>
                     </div>
                     <div className='step-container'
-                        onClick={() => setStep(2)}
-                        style={{ color: step === 2 ? "#3498db" : "#ccc" }}
+                        /*  onClick={() => setStep(2)} */
+                        style={{ color: step === 2 ? "#3498db" : complete[2] ? "#000" : "#ccc" }}
                     >
                         <div className='step-style'
-                            style={{ background: step === 2 ? "#3498db" : "#ccc", borderColor: step === 2 ? "#3498db" : "#ccc" }}
-
-                        >2</div>
+                            style={{
+                                background: step === 2 ? "#3498db" : complete[2] ? "green" : "#ccc",
+                                borderColor: step === 2 ? "#3498db" : complete[2] ? "green" : "#ccc"
+                            }}
+                        >{complete[2] ? "✓" : 2}
+                        </div>
                         <span>Job Positon</span>
                     </div>
-                    <div className='step-container' onClick={() => setStep(3)} style={{ color: step === 3 ? "#3498db" : "#ccc" }}>
-                        <div className='step-style' style={{ background: step === 3 ? "#3498db" : "#ccc", borderColor: step === 3 ? "#3498db" : "#ccc" }}
-                        >3</div>
+                    <div className='step-container' /* onClick={() => setStep(3)} */
+                        style={{ color: step === 3 ? "#3498db" : complete[3] ? "#000" : "#ccc" }}
+                    >
+                        <div className='step-style'
+                            style={{
+                                background: step === 3 ? "#3498db" : complete[3] ? "green" : "#ccc",
+                                borderColor: step === 3 ? "#3498db" : complete[3] ? "green" : "#ccc"
+                            }}
+                        >{complete[3] ? "✓" : 3}
+                        </div>
                         <span>Personal Details</span>
                     </div>
 
@@ -82,7 +109,7 @@ export default function MultiStepForm() {
                             placeholder='City,Area...'
                             {...register("location", { required: "Location Is Required!" })}
                         />
-                        {errors.location && <p>{errors.location.message}</p>}
+                        {errors.location && <p className="error-text">{errors.location.message}</p>}
                         <button type='button' className="btn btn-primary" onClick={() => nextStep(1)}>Next Step</button>
                     </div>
                 )}
@@ -93,7 +120,7 @@ export default function MultiStepForm() {
                             placeholder='Job title,Position...'
                             {...register("roles", { required: "Roles Is Required!" })}
                         />
-                        {errors.roles && <p>{errors.roles.message}</p>}
+                        {errors.roles && <p className="error-text">{errors.roles.message}</p>}
                         <button type='button' className="btn btn-primary" onClick={() => nextStep(2)}>Next Step</button>
                         <button type='button' className="btn btn-secondary" onClick={() => prevStep(2)}>Prev Step</button>
 
@@ -106,24 +133,27 @@ export default function MultiStepForm() {
                             placeholder='Name...'
                             {...register("name", { required: "Name Is Required!" })}
                         />
-                        {errors.name && <p>{errors.name.message}</p>}
+                        {errors.name && <p className="error-text">{errors.name.message}</p>}
                         <input
                             type="text"
                             placeholder='Phone...'
                             {...register("phone", { required: "Phone Is Required!" })}
                         />
-                        {errors.phone && <p>{errors.phone.message}</p>}
+                        {errors.phone && <p className="error-text">{errors.phone.message}</p>}
                         <input
                             type="text"
                             placeholder='Certification...'
                             {...register("certification", { required: "Certification Is Required!" })}
                         />
-                        {errors.certification && <p>{errors.certification.message}</p>}
+                        {errors.certification && <p className="error-text">{errors.certification.message}</p>}
 
                         <button type='button' className="btn btn-primary" onClick={() => prevStep(3)}>Prev Step</button>
 
-                        <button type='submit' className="btn btn-danger">Send</button>
+                        <button type='submit' className="btn btn-danger" >Send</button>
                     </div>
+                )}
+                {step === 4 && (
+                    <div>Success!!</div>
                 )}
             </form>
         </div>
